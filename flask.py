2dbdb10e5e1b281b1541b8c095ba14aed5ae6697
Dhruv Pahwa <96496469+Dhruv-Pahwa@users.noreply.github.com>
2026-01-03 02:59:02 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -5,5 +5,10 @@ app=Flask(__name__)
 app.route("/")
 def home():
     return "Hello, Flask!"
+    
+app.route("/index")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
